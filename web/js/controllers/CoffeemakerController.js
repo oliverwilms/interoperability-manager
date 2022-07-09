ff4fabbef3765c0bb1b221ed7fb06dff3d381dff
Oliver Wilms <50807396+oliverwilms@users.noreply.github.com>
2022-07-09 08:59:13 -0500

    Delete CoffeemakerController.js

--- web/js/controllers/CoffeemakerController.js
+++
@@ -1,7 +0,0 @@
-app.controller('CoffeemakerController', ['$scope', 'restapi', '$routeParams', function($scope, restapi, $routeParams) {
-	$scope.specs = ['color','numcups'];
-	
-	restapi.getCoffeemaker($routeParams.id).success(function(data) {
-		$scope.detail = data;
-	});
-}]);
